8c695e1c6e8765018f89362580276ebef3b08d2e
merri2017 <merrishel.richardson@gmail.com>
2018-03-29 21:30:35 -0400

    solo pokedex complete

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # pokedex2.0
+A pokemon trainer showcase website that displays information focused around 3 pokemon using AJAX to retrieve data. The focus is client side. 
+
+Mobile responsive, mobile app demo displayed in a phone frame on a webpage. 
+
+Getting Started
+Not open for development or testing yet. 
+
+Deployment
+Add additional notes about how to deploy this on a live system
+
+Built With
+HTML
+CSS/SASS
+JavaScript
+JQuery
+
+Contributing
+Coming soon
+
+Acknowledgments
+Inspiration: A mobile app pokedex for the adventurous but budget conscious pokemon trainer. 
--- pokedex.js
+++ pokedex.js
@@ -1,86 +1,115 @@
-// var myPokedex = " ";
-// $.ajax({
-// 	type:'GET',
-// 	url:"https://pokeapi.co/api/v2/pokemon/ninetales/",
-// 	success: function(data){
-// 		console.log("asd");
-// 		var name = data.forms[0].name;
-// 		var hp = data.stats[5].base_stat;
-// 		var attack = data.stats[4].base_stat;
-// 		//var abilities = [data.abilities[0].name, data.abilities[1].name];
-// 		var abilities = [];
-// 		//loop through abilities array and push to empty abilities variable.
-// 		for (i = 0; i < data.abilities.length; i++) {
-// 			//this follows api path
-// 			abilities.push(data.abilities[i].ability.name)
-// 		}
-// 		var defense = data.stats[3].base_stat;
-// 		console.log(abilities);
-// 	}
-// })
-
-
-
-
-// $(document).ready(function getPokemon (){
-// 	$.ajax({
-// 		type:'GET',
-// 		url:"https://pokeapi.co/api/v2/pokemon/ninetales/",
-// 		data: hp, attack, 
-// 		success: function(pokemon){
-// 			// I want to get the information inside of pokemon
-// 			// object and displayed
-// 		}
-
-// 	});     
-// });
-// var ninetales = new Pokemon ("name");
-// console.log(ninetales);
-
-
-class Trainer {
-	constructor(name, pokemon){
-		this.name = name;
-		this.pokemon = pokemon;
-
-	// 	function getPokemon(){
-	// 		ret;
-	}
-	
+function getNinetales(){
+	$.ajax({
+		type:'GET',
+		url:"https://pokeapi.co/api/v2/pokemon/ninetales/",
+		success: function(data){
+			var name = data.forms[0].name;
+			var hp = data.stats[5].base_stat;
+			var attack = data.stats[4].base_stat;
+			var abilities = [];
+			//loop through abilities array and push to empty abilities variable.
+			for (i = 0; i < data.abilities.length; i++) {
+				//this follows api path
+				abilities.push(data.abilities[i].ability.name)
+			}
+			var defense = data.stats[3].base_stat;
+			var ninetales = new Pokemon (name, hp, attack, defense, abilities);
+			document.getElementById("moo").innerHTML= "NINETALES";
+			document.getElementById("moo2").innerHTML= "HP"+":"+ " "+ hp;
+			document.getElementById("moo3").innerHTML= "Attack"+":"+ " "+ attack;
+			document.getElementById("moo4").innerHTML= "Defense"+":"+ " "+ defense;
+			document.getElementById("moo5").innerHTML= "Abilities"+":"+ " "+ abilities;
+		} 
+	})
 }
+getNinetales();
 
- var celina = new Trainer ("Celina");
- // console.log(celina);
+function getClefairy() {
+	$.ajax({
+		type:'GET',
+		url:"https://pokeapi.co/api/v2/pokemon/clefairy/",
+		success: function(data){
+			var name = data.forms[0].name;
+			var hp = data.stats[5].base_stat;
+			var attack = data.stats[4].base_stat;
+			var abilities = [];
+			//loop through abilities array and push to empty abilities variable.
+			for (i = 0; i < data.abilities.length; i++) {
+				//this follows api path
+				abilities.push(data.abilities[i].ability.name)
+			}
+			var defense = data.stats[3].base_stat;
+			var clefairy = new Pokemon (name, hp, attack, defense, abilities);
+			document.getElementById("dog").innerHTML= "CLEFAIRY";
+			document.getElementById("dog2").innerHTML= "HP"+ ":"+ " " + hp;
+			document.getElementById("dog3").innerHTML= "Attack"+ ":"+" "+ attack;
+			document.getElementById("dog4").innerHTML= "Defense"+ ":"+"   "+ defense;
+			document.getElementById("dog5").innerHTML= "Abilities"+":"+ " "+ abilities;
+		}
+	})
+}
+getClefairy();
 
+function getTogepi () {
+	$.ajax({
+		type:'GET',
+		url:"https://pokeapi.co/api/v2/pokemon/togepi/",
+		success: function(data){
+			var name = data.forms[0].name;
+			var hp = data.stats[5].base_stat;
+			var attack = data.stats[4].base_stat;
+			var abilities = [];
+			//loop through abilities array and push to empty abilities variable.
+			for (i = 0; i < data.abilities.length; i++) {
+				//this follows api path
+				abilities.push(data.abilities[i].ability.name)
+			}
+			var defense = data.stats[3].base_stat;
+			var togepi = new Pokemon (name, hp, attack, defense, abilities);
+			document.getElementById("bug").innerHTML= "TOGEPI";
+			document.getElementById("bug2").innerHTML= "HP"+ ":" + " "+ hp;
+			document.getElementById("bug3").innerHTML= "Attack"+ ":"+" "+attack;
+			document.getElementById("bug4").innerHTML= "Defense"+":"+ " "+defense;
+			document.getElementById("bug5").innerHTML= "Abilities"+ ":"+" "+abilities;
+		}
+	})
+}
+getTogepi();
 
 class Pokemon{
+	constructor(name, hp, attack, defense, abilities){
+		this.obj = {
+			name: name,
+			hp: hp,
+			attack: attack,
+			defense: defense,
+			abilities: abilities,
+		}
+	} 
+	returnData() {return this.obj}			
+};
+
+ var clefairy = new Pokemon ();
+ // console.log("clefairy");
+			
+
+class Trainer {
 	constructor(name){
 		this.name = name;
-		$.ajax({
-			type:'GET',
-			url:"https://pokeapi.co/api/v2/pokemon/" + name,
-			success: function(data){
-				var hp = data.stats[5].base_stat;
-				var attack = data.stats[4].base_stat;
-				// var abilities = [data.abilities[0].name, data.abilities[1].name];
-				var abilities = [];
-				//loop through abilities array and push to empty abilities variable.
-				for (var i = 0; i < data.abilities.length; i++) {
-					//this follows api path
-					abilities.push(data.abilities[i].ability.name)
-				}
-				var defense = data.stats[3].base_stat;
-								// console.log(abilities);
-				// this.name = name;
-				this.hp = hp;
-				this.attack = attack;
-				this.defense = defense;
-				this.abilities = abilities;
-				console.log(this.hp);
-			}
-		})		
+		this.pokemon=[];
+
 	}
+	add(pokemon){
+		this.pokemons.push(obj)
+	}
+displayAll() {
+    for(let i = 0; i < this.children.length; ++i) {
+      console.log(this.children[i])
+    }
 }
- var ninetales = new Pokemon ("ninetales");
- console.log(ninetales);
+}
+
+var celina = new Trainer ();
+ 
+// jquery onclick newDayCare.displayAll()
 
